c16c6b00697a5fb5f838ab36803e5c7a289052b9
Chris Donahue <christopherdonahue@gmail.com>
2018-11-22 00:00:25 -0800

    Basic thing to transcribe piano recordings in batch

---
+++ script.js
@@ -0,0 +1,32 @@
+let modelReady = false;
+const model = new mm.OnsetsAndFrames('https://storage.googleapis.com/magentadata/js/checkpoints/transcription/onsets_frames_uni');
+
+function transcribeFiles(fileList) {
+  for (let i = 0; i < fileList.length; ++i) {
+    const name = fileList[i].name;
+    console.log('Transcribing ' + name + ' ...');
+    const basename = name.split('.')[0];
+    const outputFp = basename + '.mid';
+    
+    model.transcribeFromAudioFile(fileList[i]).then((ns) => {
+      console.log('Done (' + ns.notes.length + 'notes)!');
+      saveAs(new File([mm.sequenceProtoToMidi(ns)], outputFp));
+    });
+  }
+}
+
+function initUi() {
+  const fileInput = document.getElementById('file-input');
+
+  fileInput.addEventListener('change', function (e) {
+    transcribeFiles(fileInput.files);
+  });
+  
+  document.getElementById('file-catcher').style.display = 'block';
+}
+
+model.initialize().then(() => {
+  modelReady = true;
+  initUi();
+  console.log('Ready to transcribe.');
+});
